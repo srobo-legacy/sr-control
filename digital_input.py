67e27cd2247649a70e7e149e48ef3d204247e3a5
Harry Cutts <eternal.linux@gmail.com>
2012-12-06 13:47:47 +0000

    Clean up DigitalInput code and implementation
    DigitalInput now has a set_value method, and a lot less comments (that
    were only there from the example.

--- digital_input.py
+++ digital_input.py
@@ -9,45 +9,45 @@ import pango
 from gtk import *
 
 if pygtk_version < (2, 8):
-    print "PyGtk 2.8 or later required for this example"
+    print "PyGtk 2.8 or later required"
     raise SystemExit
 
 try:
     import cairo
 except ImportError:
-    raise SystemExit("cairo required for this example")
+    raise SystemExit("cairo required")
 
-BORDER_WIDTH = 5
-
-# A quite simple gtk.Widget subclass which demonstrates how to subclass
-# and do realizing, sizing and drawing.
+_BORDER_WIDTH = 5
+_INPUT_ON_COLOR = gdk.Color(0.14453125, 0.20703125, 0.44140625)
 
 class DigitalInput(Widget):
-    def __init__(self, io_board, input_num):
-        Widget.__init__(self)
-        self.value = (78 >> input_num) & 1
-        self._layout = self.create_pango_layout('')
+
+    def set_value(self, value):
+        """Set the value displayed by the DigitalInput widget."""
+        self.value = value
 
         if self.value == 1:
             self._layout.set_markup('<span color="white">1</span>')
         else:
             self._layout.set_markup('<span color="black">0</span>')
 
-        self._layout.set_font_description(pango.FontDescription("sans bold 10"))
+    ## Constructor ##
 
-    # GtkWidget
+    def __init__(self, value):
+        Widget.__init__(self)
+        self._layout = self.create_pango_layout('')
+        self._layout.set_font_description(pango.FontDescription("sans bold 10"))
+        self.set_value(value)
+
+    ## GtkWidget methods ##
 
     def do_realize(self):
-        # The do_realize method is responsible for creating GDK (windowing system)
-        # resources. In this example we will create a new gdk.Window which we
-        # then draw on
+        # Method responsible for creating GDK (windowing system) resources. In
+        # this example we will create a new gdk.Window which we then draw on
 
-        # First set an internal flag telling that we're realized
         self.set_flags(REALIZED)
 
         # Create a new gdk.Window which we can draw on.
-        # Also say that we want to receive exposure events by setting
-        # the event_mask
         self.window = gdk.Window(
             self.get_parent_window(),
             width=self.allocation.width,
@@ -55,23 +55,21 @@ class DigitalInput(Widget):
             window_type=gdk.WINDOW_CHILD,
             wclass=gdk.INPUT_OUTPUT,
             event_mask=self.get_events() | gdk.EXPOSURE_MASK)
+            # We want exposure events
 
-        # Associate the gdk.Window with ourselves, Gtk+ needs a reference
-        # between the widget and the gdk window
+        # Associate the gdk.Window with ourselves
         self.window.set_user_data(self)
 
-        # Attach the style to the gdk.Window, a style contains colors and
-        # GC contextes used for drawing
-        # TODO: Is this the best place for style info?
+        # Attach the style to the gdk.Window. A style contains colors and
+        # GC contexts used for drawing
         self.style.attach(self.window)
 
-        # The default color of the background should be what
-        # the style (theme engine) tells us.
+        # The default background color should be what the style tells us
         self.style.set_background(self.window, STATE_NORMAL)
         self.window.move_resize(*self.allocation)
 
     def do_unrealize(self):
-        # The do_unrealized method is responsible for freeing the GDK resources
+        # responsible for freeing the GDK resources
 
         # De-associate the window we created in do_realize with ourselves
         self.window.set_user_data(None)
@@ -84,42 +82,35 @@ class DigitalInput(Widget):
         # In this case, we say that we want to be as big as the
         # text is, plus a little border around it.
         width, height = self._layout.get_size()
-        requisition.width = width // pango.SCALE + BORDER_WIDTH*4
-        requisition.height = height // pango.SCALE + BORDER_WIDTH*4
+        requisition.width = width // pango.SCALE + _BORDER_WIDTH*3
+        requisition.height = height // pango.SCALE + _BORDER_WIDTH*2
 
     def do_size_allocate(self, allocation):
         # The do_size_allocate is called by when the actual size is known
         # and the widget is told how much space could actually be allocated
 
-        # Save the allocated space
         self.allocation = allocation
 
-        # If we're realized, move and resize the window to the
-        # requested coordinates/positions
         if self.flags() & REALIZED:
             self.window.move_resize(*allocation)
 
     def do_expose_event(self, event):
         # The do_expose_event is called when the widget is asked to draw itself
-        # Remember that this will be called a lot of times, so it's usually
-        # a good idea to write this code as optimized as it can be, don't
-        # Create any resources in here.
 
-        # In this example, draw a rectangle in the foreground color
         x, y, w, h = self.allocation
         cr = self.window.cairo_create()
 
-        # Draw rounded rectangle
         if self.value == 1:
-            cr.set_source_color(gdk.Color(0.14453125, 0.20703125, 0.44140625))
-            cr.rectangle(BORDER_WIDTH, BORDER_WIDTH,
-                         w - 2*BORDER_WIDTH, h - 2*BORDER_WIDTH)
+            # Draw a filled rounded rectangle
+            cr.set_source_color(_INPUT_ON_COLOR)
+            cr.rectangle(_BORDER_WIDTH, _BORDER_WIDTH,
+                         w - 2*_BORDER_WIDTH, h - 2*_BORDER_WIDTH)
             cr.fill_preserve()
             cr.set_line_width(5.0)
             cr.set_line_join(cairo.LINE_JOIN_ROUND)
             cr.stroke()
 
-        # And draw the text in the middle of the allocated space
+        # draw the value in the centre of the control 
         fontw, fonth = self._layout.get_pixel_size()
         cr.move_to((w - fontw)/2, (h - fonth)/2)
         cr.update_layout(self._layout)
