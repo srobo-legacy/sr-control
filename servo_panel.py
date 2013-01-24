f98a54ede26bf2b44b92b7322e54ab955b834b1c
Harry Cutts <eternal.linux@gmail.com>
2013-01-24 15:50:33 +0000

    Distinguish between set and unset servos
    If a servo has not been set since the board was powered up, its slider
    will appear orange and be set to 50. To set that servo, the user must
    change the slider to the desired value and press the rotary encoder.
    This is to prevent mechanical damage to the servo or the mechanism to
    which it is connected.

--- servo_panel.py
+++ servo_panel.py
@@ -10,6 +10,11 @@ NUM_SERVOS = 8
 
 _SLIDER_NOTCH = 1
 
+_UNSET_SERVO_COLOR = gdk.Color(1.0, 0.5, 0.0)  # Orange
+
+_SET_SERVO_COLOR = gdk.Color(0.14453125, 0.20703125, 0.44140625)
+_SET_SERVO_TEXT_COLOR = gdk.Color(0.0, 0.0, 0.0)
+
 class ServoPanel(Table):
     ## Servo selection and manipulation ##
 
@@ -29,11 +34,12 @@ class ServoPanel(Table):
 
     def set_servo(self, num, value):
         self.sliders[num].set_value(value)
-        if self.board != None:
+        if self.board != None and self.servo_set[num]:
             self.board[num] = value
+            print "Output",num,"set."
 
     def change_servo(self, num, notches):
-        value = self.board[num]
+        value = self.sliders[num].get_value()
         self.set_servo(num, int(value) + notches * _SLIDER_NOTCH)
 
     ## Event handlers ##
@@ -45,6 +51,13 @@ class ServoPanel(Table):
         elif event.keyval == keysyms.Down:
             self.change_servo(self.selected_servo, -1)
 
+        elif event.keyval == keysyms.Return:
+            num = self.selected_servo
+            self.servo_set[num] = True
+            self.set_servo(num, self.sliders[num].get_value())
+            self.sliders[num].modify_bg(STATE_NORMAL, _SET_SERVO_COLOR)
+            self.sliders[num].modify_fg(STATE_NORMAL, _SET_SERVO_TEXT_COLOR)
+
         elif event.keyval == keysyms.Page_Up:
             self.prev_servo()
 
@@ -53,12 +66,20 @@ class ServoPanel(Table):
 
     def panel_update(self, _):
         for i in range(NUM_SERVOS):
-            self.sliders[i].set_value(self.board[i])
+            value = self.board[i]
+            if self.servo_set[i]:
+                self.sliders[i].set_value(value)
+            else:
+                if value <= 100:
+                    self.sliders[i].modify_bg(STATE_NORMAL, _SET_SERVO_COLOR)
+                    self.sliders[i].modify_fg(STATE_NORMAL, _SET_SERVO_TEXT_COLOR)
+                    self.servo_set[i] = True
 
     ## Constructor ##
 
     labels = []
     sliders = []
+    servo_set = []
 
     def __init__(self, board = None):
         Table.__init__(self, 3, NUM_SERVOS)
@@ -81,11 +102,16 @@ class ServoPanel(Table):
             self.labels.append(SelectableLabel(str(i)))
             self.attach(self.labels[i], i, i + 1, 1, 2, yoptions=SHRINK)
             # Slider
-            adj = Adjustment(value = 0, lower = 0, upper = 100)
-            self.sliders.append(VScale(adj))
-            self.sliders[i].set_inverted(True)
-            self.sliders[i].set_value_pos(POS_BOTTOM)
-            self.attach(self.sliders[i], i, i + 1, 2, 3)
+            adj = Adjustment(value = 50, lower = 0, upper = 100)
+            slider = VScale(adj)
+            slider.set_inverted(True)
+            slider.set_value_pos(POS_BOTTOM)
+            self.attach(slider, i, i + 1, 2, 3)
+
+            slider.modify_fg(STATE_NORMAL, _UNSET_SERVO_COLOR)
+            slider.modify_bg(STATE_NORMAL, _UNSET_SERVO_COLOR)
+            self.sliders.append(slider)
+            self.servo_set.append(False)
 
         self.labels[self.selected_servo].set_state(STATE_SELECTED)
 
@@ -100,3 +126,4 @@ class ServoPanel(Table):
         if board != None:
             self.panel_update(None)
             self.connect("panel-update", self.panel_update)
+
