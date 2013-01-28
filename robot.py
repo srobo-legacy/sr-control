60fccb8b9f32266fbc1a43f58adaae6c1f84c013
Harry Cutts <eternal.linux@gmail.com>
2013-01-28 16:19:23 +0000

    Add red stop button to Controller window
    This button appears when one or more motors are running (whether or not
    they have been set running by the Controller). When the left knob is
    pressed, the button on-screen appears depressed too. This is to provide
    a hint that the left knob stops motors when pressed.

--- robot.py
+++ robot.py
@@ -15,6 +15,7 @@ from panels import create_panel_by_class_name, create_panel
 UPDATE_FREQUENCY = 1000
 
 class Controller:
+
     ## Internal methods ##
 
     def display_panel(self, panel):
@@ -68,6 +69,7 @@ class Controller:
             self.select_next_panel()
 
         elif event.keyval == keysyms.space:
+            self.stop_button.set_state(STATE_ACTIVE)
             # Stop all motors
             if self.robot != None:
                 for i in range(len(self.robot.motors)):
@@ -77,6 +79,10 @@ class Controller:
             self.current_panel.emit("key-press-event", event)
 
         return True  # stop the event from triggering stuff we don't want
+
+    def key_release(self, widget, event):
+        if event.keyval == keysyms.space:
+           self.stop_button.set_state(STATE_NORMAL)
 
     def selected_panel_changed(self, tree_selection):
         sel_iter = self.panel_list.get_selection().get_selected()[1]
@@ -98,11 +104,26 @@ class Controller:
         self.timer_on = True
 
     def timer_tick(self):
-        """Emits an update signal at the current panel."""
+        """Emits an update signal at the current panel, and checks the number
+           of running motors."""
         if not self.timer_on:
             return False
 
         self.current_panel.emit("panel-update")
+
+        # Check to see whether motors are running
+        if self.robot != None:
+            motor_running = False
+            for i in range(len(self.robot.motors)):
+                if self.robots.motors[i].target != 0:
+                    motor_running = True
+                    break
+
+            if motor_running:
+                self.stop_button.hide()
+            else:
+                self.stop_button.show()
+
         return True
 
     def stop_timer(self):
@@ -156,15 +177,23 @@ class Controller:
         tvcol.set_attributes(cell, text=0)
         first_path = panel_list_store.get_path(panel_list_store.get_iter_first())
         panel_list.get_selection().select_path(first_path)
-        panel_list.show()
+
+        stop_button = Button("Press to stop")
+        stop_button.modify_bg(STATE_NORMAL, gdk.Color(1.0, 0.2, 0.2))
+        self.stop_button = stop_button
+
+        left_table = Table(2, 1, False)
+        left_table.attach(panel_list, 0, 1, 0, 1)
+        left_table.attach(stop_button, 0, 1, 1, 2, yoptions=SHRINK)
 
         # Horizontal paning
         hpaned = HPaned()
-        hpaned.pack1(panel_list)
+        hpaned.pack1(left_table)
         hpaned.set_position(128)
 
         window.add(hpaned)
         window.show_all()
+        stop_button.hide()
 
         # Event handlers
         window.connect("delete_event", self.delete_event)
@@ -172,6 +201,7 @@ class Controller:
         window.connect("focus-in-event", self.get_focus)
         window.connect("focus-out-event", self.lose_focus)
         window.connect("key-press-event", self.key_press)
+        window.connect("key-release-event", self.key_release)
 
         panel_list.get_selection().connect("changed", self.selected_panel_changed)
 
